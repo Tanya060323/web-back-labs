776dd72b67ad4b2bd2cd7308e45641bc38dc8534
Татьяна Щегорцова <tttaaannnaaa62@gmail.com>
2025-10-01 03:23:08 +0700

    Имя студента

--- app.py
+++ app.py
@@ -387,4 +387,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Татьяна Щегорцова'
+    return render_template('example.html', name=name)
