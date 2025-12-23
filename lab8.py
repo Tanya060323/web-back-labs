936da3281847cc695a4913d642c03ebb35461c55
Татьяна Щегорцова <tttaaannnaaa62@gmail.com>
2025-12-24 00:50:59 +0700

    Функция запомни меня

--- lab8.py
+++ lab8.py
@@ -8,7 +8,11 @@ lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def main():
-    return render_template('lab8/lab8.html')
+    if current_user.is_authenticated:
+        login = current_user.login  
+    else:
+        login = 'Anonymous'
+    return render_template('lab8/lab8.html', login=login)
 
 
 @lab8.route('/lab8/register/', methods = ['GET', 'POST'])
